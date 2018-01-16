0a86561ad4b48a8297b1bf7f5c4d04ac9fd898bd
Vayam Jain <vjain@grepruby.com>
2018-01-16 19:07:29 +0530

    Module exports

---
+++ Mdule and exports/oneModule.js
@@ -0,0 +1,14 @@
+//question-one
+var inputOne = [1, 2, 3, 4, 5];
+var callingFirst = require('./assignment1-one');
+console.log(callingFirst.secondHighest(inputOne));
+
+//question-two
+var inputTwo = "asdwqdaabbwwddssafhjkls";
+var callingTwo = require('./assignment1-two');
+callingTwo.frequencyCounter(inputTwo);
+
+//question-three
+var inputThree = "cecream";
+var callingThree = require('./assignment1-three');
+callingThree.printChecker(inputThree);
