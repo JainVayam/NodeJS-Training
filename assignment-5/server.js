913b4d6f372b5fb0ebe7ad24f8c30755aaf0f854
Vayam Jain <vjain@grepruby.com>
2018-01-22 16:19:44 +0530

    Second addition

--- assignment-5/server.js
+++ assignment-5/server.js
@@ -1,20 +1,25 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+
+var gettingUser = require('./getUser');
+var deletingUser = require('./deleteUser');
+var updatingUser = require('./updateUser');
+var insertingUser = require('./insertUser');
+
+var app = express();
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended : true })); 
+
 var user = [ 
-  {fname : "Ron", lname : "Weas", id : 1},
-  {fname : "Har", lname : "Pott", id : 2}
+  {id : 1, fname : "Ron", lname : "Weas"},
+  {id : 2, fname : "Har", lname : "Pott"}
 ];
-var gettingApp = require('./getApp');
-gettingApp.getApp(app, user);
-var deletingApp = require('./deleteApp');
-deletingApp.deleteApp(app, user);
-var updatingApp = require('./updateApp');
-updatingApp.updateApp(app, user);
-var insertingApp = require('./insertApp');
-insertingApp.insertApp(app, user);
+
+gettingUser.getUser(app, user);
+insertingUser.insertUser(app, user);
+updatingUser.updateUser(app, user);
+deletingUser.deleteUser(app, user);
+
 var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
