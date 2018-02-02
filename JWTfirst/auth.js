b281b76ddbb1c8a831d2ba7be7769c6050625645
Vayam Jain <vjain@grepruby.com>
2018-02-02 19:52:15 +0530

    JWT Tokens

---
+++ JWTfirst/auth.js
@@ -0,0 +1,32 @@
+const passport = require("passport");  
+const passportJWT = require("passport-jwt");  
+const users = require("./usersList.js");  
+const cfg = require("./config.js");  
+const ExtractJwt = passportJWT.ExtractJwt;  
+const Strategy = passportJWT.Strategy;  
+const params = {  
+    secretOrKey: cfg.jwtSecret,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt")
+};
+         
+module.exports = function() {  
+  const strategy = new Strategy(params, ((payload, done) => {
+    const user = users[payload.id] || null;
+    if (user) {
+      return done(null, {
+        id: user.id
+      });
+    } else {
+      return done(new Error("User not found"), null);
+    }
+  }));
+  passport.use(strategy);
+  return {
+    initialize: function() {
+    return passport.initialize();
+  },
+    authenticate: function() {
+      return passport.authenticate("jwt", cfg.jwtSession);
+    }
+  };
+};
